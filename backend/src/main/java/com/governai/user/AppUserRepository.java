4e903fdff308e947f4143555a5389a96e01c5be3
Ajit Pattepu <pattepuajit23@gmail.com>
2026-08-29 17:37:40 +0530

    Commit refactored code

--- backend/src/main/java/com/governai/user/AppUserRepository.java
+++ backend/src/main/java/com/governai/user/AppUserRepository.java
@@ -1,9 +1,18 @@
 package com.governai.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    @Query("""
+        SELECT u
+        FROM AppUser u
+        JOIN FETCH u.organization
+        WHERE LOWER(u.email) = LOWER(:email)
+    """)
     Optional<AppUser> findByEmailIgnoreCase(String email);
+
     boolean existsByEmailIgnoreCase(String email);
 }
